e561fb0476253bb62838a3907f9ecb96d662afab
Heather Brand-Williamson <117382879+heatherbrandwilliamson@users.noreply.github.com>
2023-03-15 13:01:57 +0000

    Add files via upload

---
+++ intials.py
@@ -0,0 +1,9 @@
+#hello here are my initials
+
+print("H    H BBBB ")
+print("H    H B    B")
+print("H    H B    B")
+print("HHHHHH BBBB ")
+print("H    H B    B")
+print("H    H B    B")
+print("H    H BBBB")
